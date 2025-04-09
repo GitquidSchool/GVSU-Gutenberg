b71822e3176593b4e0c724dd3e7b9a1e34902ec5
ethan-leong256 <ethan.umana2@gmail.com>
2025-04-09 13:54:13 -0400

    Add files via upload
    Starter code

---
+++ gutenberg.js
@@ -0,0 +1,28 @@
+const url = 'https://gutendex.com/books?search=';
+ 
+async function getData(str){
+  const request = await fetch(url + str);
+  const json = await request.json();
+  // console.log(json)
+  for(let i=0; i<json.count; ++i){
+    console.log(json.results[i].title+ " - " + json.results[i].id);
+  }
+  return;
+}
+
+
+// choose way to denote choosen book
+// pull book down from project gutenberg
+// print full text
+// https://www.gutenberg.org/ebooks/bookid
+
+const url2 = 'https://www.gutenberg.org/ebooks/';
+// doesnt work yet
+async function getBook(url2, id){
+    const request = await fetch(url2 + id);
+    const json = await request.json();
+    console.log(json)
+};
+
+getData("sherlock");
+// getBook(1661)
