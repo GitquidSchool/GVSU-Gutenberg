5b87b2355887956e54e20917cec415bb79191172
Ethan <umanae@mail.gvsu.edu>
2025-04-12 20:13:42 -0400

    made view reading history in menu.js

--- menu.js
+++ menu.js
@@ -2,6 +2,7 @@ const readline = require('readline');
 const { safeFetch, fetchJSON, fetchText, printBookTitles, ask } = require('./utils');
 const { readBook } = require('./reader');
 const { search } = require('./search')
+const { loadHistory, saveHistory, addToHistory, listHistory, getBookFromHistory } = require('./history')
 
 
 // this what I was thinking for displaying, JUST ROUGH DRAFT THO:
@@ -17,32 +18,32 @@ async function menu() {
         input: process.stdin,
         output: process.stdout
     });
+    while (true) {
+        console.log('=== Project Gutenberg Reader ===');
+        console.log('1. Search for a book');
+        console.log('2. View reading history');
+        console.log('3. Re-read a book from history');
+        console.log('4. Quit');
 
-    console.log('=== Project Gutenberg Reader ===')
-    console.log('1. Search for a book')
-    console.log('2. View reading history')
-    console.log('3. Re-read a book from history')
-    console.log('4. Quit')
-
-    const choice = await ask(rl, 'Select an Option: ')
-    if (choice === '1'){
-        rl.close();
-        await search();
-    }
-    if (choice === '2'){
-        console.log('view history')
-        rl.close();
-        return;
-    }
-    if (choice === '3'){
-        console.log('Reread book')
-        rl.close();
-        return;
-    }
-    if (choice === '4'){
-        console.log('quit')
-        rl.close();
-        return;
+        const choice = await ask(rl, 'Select an Option: ');
+        if (choice === '1'){
+            rl.close();
+            await search();
+        }
+        if (choice === '2'){
+            await listHistory();
+            continue
+        }
+        if (choice === '3'){
+            console.log('Reread book');
+            rl.close();
+            return;
+        }
+        if (choice === '4'){
+            console.log('quit');
+            rl.close();
+            return;
+        }
     }
 }
 
